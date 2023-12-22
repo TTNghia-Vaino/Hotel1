33ba2328d698a6ee176d7029805795030507dd14
TTNghia <tatrongnghiak15@siu.edu.vn>
2023-12-22 14:47:25 +0700

    Done Khachangs

---
+++ Hotel1/Controllers/KhachhangsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Hotel1.Data;
+using Hotel1.Models;
+
+namespace Hotel1.Controllers
+{
+    public class KhachhangsController : Controller
+    {
+        private readonly DpContext _context;
+
+        public KhachhangsController(DpContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Khachhangs
+        public async Task<IActionResult> Index()
+        {
+              return _context.Khachhangs != null ? 
+                          View(await _context.Khachhangs.ToListAsync()) :
+                          Problem("Entity set 'DpContext.Khachhangs'  is null.");
+        }
+
+        // GET: Khachhangs/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Khachhangs == null)
+            {
+                return NotFound();
+            }
+
+            var khachhang = await _context.Khachhangs
+                .FirstOrDefaultAsync(m => m.Cmnd == id);
+            if (khachhang == null)
+            {
+                return NotFound();
+            }
+
+            return View(khachhang);
+        }
+
+        // GET: Khachhangs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Khachhangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Cmnd,Ten,Ngaysinh,Diachi,Phai,Sdt,Email,Malk")] Khachhang khachhang)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(khachhang);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(khachhang);
+        }
+
+        // GET: Khachhangs/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Khachhangs == null)
+            {
+                return NotFound();
+            }
+
+            var khachhang = await _context.Khachhangs.FindAsync(id);
+            if (khachhang == null)
+            {
+                return NotFound();
+            }
+            return View(khachhang);
+        }
+
+        // POST: Khachhangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Cmnd,Ten,Ngaysinh,Diachi,Phai,Sdt,Email,Malk")] Khachhang khachhang)
+        {
+            if (id != khachhang.Cmnd)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(khachhang);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KhachhangExists(khachhang.Cmnd))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(khachhang);
+        }
+
+        // GET: Khachhangs/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Khachhangs == null)
+            {
+                return NotFound();
+            }
+
+            var khachhang = await _context.Khachhangs
+                .FirstOrDefaultAsync(m => m.Cmnd == id);
+            if (khachhang == null)
+            {
+                return NotFound();
+            }
+
+            return View(khachhang);
+        }
+
+        // POST: Khachhangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Khachhangs == null)
+            {
+                return Problem("Entity set 'DpContext.Khachhangs'  is null.");
+            }
+            var khachhang = await _context.Khachhangs.FindAsync(id);
+            if (khachhang != null)
+            {
+                _context.Khachhangs.Remove(khachhang);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KhachhangExists(string id)
+        {
+          return (_context.Khachhangs?.Any(e => e.Cmnd == id)).GetValueOrDefault();
+        }
+    }
+}
--- Hotel1/Models/Khachhang.cs
+++ Hotel1/Models/Khachhang.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hotel1.Models;
@@ -19,6 +20,7 @@ public partial class Khachhang
     public string Ten { get; set; } = null!;
 
     [Column("NGAYSINH", TypeName = "date")]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
     public DateTime Ngaysinh { get; set; }
 
     [Column("DIACHI")]
@@ -28,17 +30,18 @@ public partial class Khachhang
     public bool Phai { get; set; }
 
     [Column("SDT")]
-    [StringLength(11)]
+    [StringLength(10)]
     [Unicode(false)]
-    public string Sdt { get; set; } = null!;
+    public string? Sdt { get; set; } = null;
 
     [Column("EMAIL")]
     [StringLength(200)]
     [Unicode(false)]
-    public string Email { get; set; } = null!;
+    public string? Email { get; set; } = null;
 
     [Column("MALK")]
     [StringLength(20)]
+    [Unicode(true)]
     public string Malk { get; set; } = null!;
 
     [ForeignKey("Cmnd")]
